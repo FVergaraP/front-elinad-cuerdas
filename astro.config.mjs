a775683b73f13e9556243543b067519c597f23b6
fjvergarap <fco.vergara.pena@gmail.com>
2026-06-04 18:09:00 -0400

    feat: posicionamiento google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: 'https://fvergarap.github.io',
   base: '/front-elinad-cuerdas',
+  integrations: [sitemap()],
 });
